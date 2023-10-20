3a50abf0aa229482e0aacc15bd251b86c8d4a4cd
Alvin Galit <aroblesgalit@gmail.com>
2023-10-19 19:48:54 -0700

    Added sign out button

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,20 +15,27 @@ const Nav = () => {
           <Image 
             src='/assets/images/superheroes-icon-white.svg'
             alt='Superheroes logo'
-            width={32}
-            height={32}
+            width={40}
+            height={40}
           />
-          <p className='font-anton text-primary-white text-xl max-sm:hidden'>Superheroes</p>
+          <p className='font-anton text-primary-white text-2xl max-sm:hidden'>Superheroes</p>
         </Link>
 
         {/* Desktop navigation */}
         <div className='sm:flex hidden'>
           {
             isUserLoggedIn ? (
-              <div className='flex items-center gap-3 md:gap-5 text-primary-white'>
+              <div className='flex items-center gap-3 md:gap-5 text-primary-white text-sm'>
                 <Link href='/collection'>
                   Collection
                 </Link>
+                <button
+                  type='button'
+                  onClick={signOut}
+                  className='border rounded-full px-5 py-1.5 transition-all hover:bg-primary-black hover:border-primary-black'
+                >
+                  Sign out
+                </button>
               </div>
             ) : (
               <></>
