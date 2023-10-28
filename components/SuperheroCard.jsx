9e84d658659d2c6586f98b5e60e46f8dc34f2d55
Alvin Galit <aroblesgalit@gmail.com>
2023-10-28 13:50:56 -0700

    Listed props for superhero card

--- components/SuperheroCard.jsx
+++ components/SuperheroCard.jsx
@@ -5,3 +5,21 @@ const SuperheroCard = () => {
 }
 
 export default SuperheroCard
+
+// appearance > race: STRING
+//            > gender: STRING
+//            > height: ARRAY - 0: ft in | 1: cm
+//            > weight: ARRAY - 0: lb | 1: kg
+// biography > aliases: ARRAY
+//           > alignment: STRING
+//           > full-name: STRING
+//           > publisher: STRING
+// id: STRING
+// image > url: STRING
+// name: STRING
+// powerstats > combat: STRING
+//            > durability: STRING
+//            > intelligence: STRING
+//            > power: STRING
+//            > speed: STRING
+//            > strength: STRING
