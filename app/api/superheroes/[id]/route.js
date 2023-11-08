b26a0850117f616afa769b3c3e01b47b43d347e5
Alvin Galit <aroblesgalit@gmail.com>
2023-11-07 20:18:06 -0800

    Deleted superhero api route for adding new superhero to DB and moved it to existing api route

--- app/api/superheroes/[id]/route.js
+++ app/api/superheroes/[id]/route.js
@@ -13,3 +13,17 @@ export const GET = async (request, { params }) => {
     return new Response('Internal server error', { status: 500 });
   }
 }
+
+export const POST = async (request) => {
+  const { id, name, image, appearance, biography, powerstats, connections, work } = await request.json();
+
+  try {
+    await connectToDB();
+    const newSuperhero = new Superhero({ id, name, image, appearance, biography, powerstats, connections, work });
+
+    await newSuperhero.save();
+    return new Response(JSON.stringify(newSuperhero), { status: 201 });
+  } catch (error) {
+    return new Response('Failed to add superhero to database', { status: 500 });
+  }
+}
